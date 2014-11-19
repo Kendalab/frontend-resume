dc7eb6a762b2d96b8604f00dd39cae01a94b22d8
Adam Kendal <kendal.adam@mail.com>
2014-11-19 14:27:06 -0500

    Create a new resumeBuilder.js and create four JSON objects in that file (work, projects, education and bio)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,23 +1,43 @@
-// $("#header").prepend(formattedRole)
-// $("#header").prepend(formattedName)
-
 var bio = {
 	"name": "Adam Kendal",
 	"role": "Front-End Web Developer",
 	"contacts": {
 		"mobile" : "647-802-6755",
-	"email": "kendal.adam@gmail.com",
+		"email": "kendal.adam@gmail.com",
+		"github": "Kendalab",
+		"twitter": "Kendalab",
+		"location": "Toronto, Ontario"
 	},
 	"pictureURL": "https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/10403571_10202745620315741_8553441052524735893_n.jpg?oh=a99fefbab1fac54b388caed506e6c8e2&oe=54E1D402&__gda__=1424062845_2a9dab8ecf8a23aeb8f5b44532f0ecd7",
 	"welcome": "Welcome to my resume",
 	"skills": ["HTML5", "CSS3", "JavaScript"]
-}
+};
 
-var work = {};
-work.position = "Graduate Student Researcher";
-work.employer = "Douglas Hospital";
-work.yearsWorked = "2";
-work.city = "Montreal";
+var work = {
+	"jobs": [
+		{
+			"employer": "Douglas Mental Health Hospital",
+			"title": "Graduate Student Researcher",
+			"location": "Montreal, Quebec",
+			"dates": "2012-2014",
+			"description": "Performed neuropsychological research relating to guilt, depression and suicide."
+		},
+		{
+			"employer": "Rotman Research Institute at Baycrest",
+			"title": "Summer Student Researcher",
+			"location": "Toronto, Ontario",
+			"dates": "Summer 2012",
+			"description": "Performed neuropsychological research relating to vision, audition and aging."
+		},
+		{
+			"employer": "Turner Fliescher Architects Inc.",
+			"title": "Architectual Technician",
+			"location": "Toronto, Ontario",
+			"dates": "Summer 2010",
+			"description": "Designed site plans for various commercial clients."
+		}
+	]
+};
 
 var education = {
 	"schools": [
@@ -48,27 +68,30 @@ var education = {
 	]
 };
 
- 
+var projects = {
+	"projects": [
+		{
+			"title": 9,
+			"dates": 9,
+			"description": 9,
+			"images": 9
+		},
+				{
+			"title": 9,
+			"dates": 9,
+			"description": 9,
+			"images": 9
+		},
+				{
+			"title": 9,
+			"dates": 9,
+			"description": 9,
+			"images": 9
+		}
+	]
+};
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedPicture = HTMLbioPic.replace("%data%", bio.pictureURL);
-var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcome);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-var formattedTitle = HTMLworkTitle.replace("%data%", work["position"]);
-var formattedSchool = HTMLschoolName.replace("%data%", education.name);
 
 
 
 
-$("#main").append(formattedSchool)
-$("#main").append(formattedTitle)
-$("#header").prepend(formattedSkills)
-$("#header").prepend(formattedWelcome)
-$("#header").prepend(formattedPicture)
-$("#header").prepend(formattedEmail)
-$("#header").prepend(formattedMobile)
-$("#header").prepend(formattedRole)
-$("#header").prepend(formattedName)
