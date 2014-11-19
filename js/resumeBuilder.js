066052acb1c73c133be59cfcc306942c93a8255f
Adam Kendal <kendal.adam@mail.com>
2014-11-19 12:31:28 -0500

    Create bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,37 @@
-$("#main").append([yourname])
+// $("#header").prepend(formattedRole)
+// $("#header").prepend(formattedName)
+
+var bio = {
+	"name": "Adam Kendal",
+	"role": "Front-End Web Developer",
+	"contacts": {
+		"mobile" : "647-802-6755",
+		"email": "kendal.adam@gmail.com",
+	},
+	"pictureURL": "https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/10403571_10202745620315741_8553441052524735893_n.jpg?oh=a99fefbab1fac54b388caed506e6c8e2&oe=54E1D402&__gda__=1424062845_2a9dab8ecf8a23aeb8f5b44532f0ecd7",
+	"welcome": "Welcome to my resume",
+	"skills": ["HTML5", "CSS3", "JavaScript"]
+}
+
+bio.work.currentPosition = "Graduate Student Researcher";
+bio.work.employer = "Douglas Hospital";
+bio.work.yearsWorked = "2";
+bio.work.city = "Montreal";
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedPicture = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcome);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+
+$("#header").prepend(formattedSkills)
+$("#header").prepend(formattedWelcome)
+$("#header").prepend(formattedPicture)
+$("#header").prepend(formattedEmail)
+$("#header").prepend(formattedMobile)
+$("#header").prepend(formattedRole)
+$("#header").prepend(formattedName)
